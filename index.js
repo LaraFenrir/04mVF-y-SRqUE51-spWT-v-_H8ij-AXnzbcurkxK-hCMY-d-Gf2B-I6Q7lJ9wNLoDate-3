0df1be0e5dedbc77e80024de1bd959f35508a826
yoann653 <39006711+yoann653@users.noreply.github.com>
2018-05-06 00:46:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -67,11 +67,11 @@ let params  = cmdParams;
      message.edit("Bot : " + Math.round(endTime - startTime) + " ms\nAPI : "+Math.round(bot.ping)+" ms");
     })
 }
-if(msg.content === prefix + join){
+if(msg.content === prefix + "join"){
 Music.cmdMusic("join", msg, params.join(""), bot)
 }
-if(msg.content === prefix + play){
-Music.cmdMusic("play", msg, params.play(""), bot)
+if(msg.content === prefix + "play"){
+Music.cmdMusic("play", msg, params.join(""), bot)
 }
 });
 
