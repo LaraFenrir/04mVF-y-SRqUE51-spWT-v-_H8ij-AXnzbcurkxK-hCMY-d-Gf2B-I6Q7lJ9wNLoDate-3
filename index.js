26beca74ed4a4f30380cf0728b09d389b80eab1d
yoann653 <39006711+yoann653@users.noreply.github.com>
2018-05-06 02:14:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ let params  = cmdParams;
     })
 }
 if(msg.content === prefix + "play"){
-Music.cmdMusic("play", msg, params.join(""), bot)
+    if(params.join(" ").length === 0) return msg.channel.send("Please include a title or link");
+Music.cmdMusic("play", msg, params.join(" "), bot);
 }
 if(msg.content === prefix + "join"){
 Music.cmdMusic("join", msg, params.join(""), bot)
