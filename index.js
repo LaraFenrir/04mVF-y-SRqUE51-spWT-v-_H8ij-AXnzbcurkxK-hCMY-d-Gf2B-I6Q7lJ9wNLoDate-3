f45e5175b8cb11f2a8f2b6cb576061de83d70c43
yoann653 <39006711+yoann653@users.noreply.github.com>
2018-05-11 01:24:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,23 +60,24 @@ bot.on('message', msg => {
 let content   = msg.content;
 let cmdName   = content.split(' ')[0].toLowerCase();
 let cmdParams = content.substring(cmdName.length + 1).split(' ');
-let params  = cmdParams;
-if(msg.content === prefix + "play"){
-    if(params.join(" ").length === 0) return msg.channel.send("Please include a title or link");
-Music.cmdMusic("play", msg, params.join(" "), bot);
-}
-if(msg.content === prefix + "join"){
-Music.cmdMusic("join", msg, params.join(""), bot)
-}
-if(msg.content === prefix + "leave"){
-Music.cmdMusic("leave", msg, params.join(""), bot)
-}
-	   if (msg.content === (prefix+"ping")) {
+console.log(cmdParams);
+    if (msg.content === (prefix+"ping")) {
      var startTime = Date.now();
   msg.channel.sendMessage("Calcul en cours...").then((message) => {
    var endTime = Date.now();
      message.edit("Bot : " + Math.round(endTime - startTime) + " ms\nAPI : "+Math.round(bot.ping)+" ms");
     })
+}
+if(msg.content.startsWith(prefix + "play")){
+    if(cmdParams.join(" ").length === 0) return msg.channel.send("Please include a title or link");
+Music.cmdMusic("play", msg, cmdParams.join(""), bot);
+}
+if(msg.content === prefix + "join"){
+    console.log("merde");
+Music.cmdMusic("join", msg, cmdParams.join(""), bot)
+}
+if(msg.content === prefix + "leave"){
+Music.cmdMusic("leave", msg, cmdParams.join(""), bot)
 }
 });
 
