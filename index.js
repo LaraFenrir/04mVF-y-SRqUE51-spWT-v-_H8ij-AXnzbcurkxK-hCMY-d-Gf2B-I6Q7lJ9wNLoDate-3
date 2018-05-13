d888761bf5b21e90d3618bdfb75d9c28e65553cf
yoann653 <39006711+yoann653@users.noreply.github.com>
2018-05-13 20:05:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,13 +61,6 @@ let content   = msg.content;
 let cmdName   = content.split(' ')[0].toLowerCase();
 let cmdParams = content.substring(cmdName.length + 1).split(' ');
 
-if (msg.content === (prefix+"ping")) {
-     var startTime = Date.now();
-  msg.channel.sendMessage("Calcul en cours...").then((message) => {
-   var endTime = Date.now();
-     message.edit("Bot : " + Math.round(endTime - startTime) + " ms\nAPI : "+Math.round(bot.ping)+" ms");
-    })
-}
 if(msg.content.startsWith(prefix + "play")){
     if(cmdParams.join(" ").length === 0) return msg.channel.send("Please include a title or link");
 Music.cmdMusic("play", msg, cmdParams.join(""), bot);
@@ -106,6 +99,13 @@ Music.cmdMusic("resume", msg, cmdParams.join(""), bot)
 }
 if(msg.content === prefix + "skip"){
 Music.cmdMusic("skip", msg, cmdParams.join(""), bot)
+}
+if (msg.content === (prefix+"ping")) {
+     var startTime = Date.now();
+  msg.channel.sendMessage("Calcul en cours...").then((message) => {
+   var endTime = Date.now();
+     message.edit("Bot : " + Math.round(endTime - startTime) + " ms\nAPI : "+Math.round(bot.ping)+" ms");
+    })
 }
 });
 
